bf6ff855b998f55961763ba2b06bf03ea3b73087
CuriosityWang <50990182+CuriosityWang@users.noreply.github.com>
2020-10-26 19:50:04 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,35 @@
+<script>
+	export default {
+		
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	
+	.logo {
+		height: 300rpx;
+		width: 300rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+	
+	.text-area {
+		display: flex;
+		justify-content: center;
+		line-height: 55rpx;
+	}
+	
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
